bb6b80de16dab3110bf65c909362f3f56c2f77c2
vijaybaghel1041 <vijaybaghel1041@gmail.com>
2026-02-04 09:26:42 +0530

    Update aPi add for form modification

--- src/main/java/com/company/vse/controller/VseController.java
+++ src/main/java/com/company/vse/controller/VseController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +38,17 @@ public class VseController {
     public void delete(@PathVariable Long id) {
         service.delete(id);
     }
+    
+ // VseController.java
+
+    @PutMapping("/{id}")
+    public VseSubmission update(
+            @PathVariable Long id,
+            @RequestBody VseSubmission vse) {
+
+        vse.setId(id);
+        return service.save(vse);
+    }
+
 }
 
